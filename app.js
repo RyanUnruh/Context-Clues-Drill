35839888beb205b0173918ffcbdee59861692f7d
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-02-04 11:43:44 -0600

    FINAL

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  var friend = ['Dan', 'Eric', 'Sue', 'Jill', 'Rae'];
+  var weapon = ['Hammer', 'Blade', 'Brick', 'Sward', 'Gun', 'Bat', 'Bar', 'Shoe', 'Crowbar', 'bottle', 'Pin', 'Sand', 'Paper', 'computer', 'nail gun', 'glass', 'chair', 'waggon', 'car', 'truck', 'van'];
+  var location = ['Kansas', 'Montana', 'CA', 'NY', 'FL', 'Main', 'Tower', 'Downtown', 'road', 'Inside'];
 
-for (let index = 0; index < 100; index++) {
-  
- 
+
+  for (i = 1; i < 100; i++) {
     var h3 = document.createElement('h3');
-    var h3Txt = document.createTextNode(`Accusation ${index}`);
+    var h3Txt = document.createTextNode(`Accusation ${i}`);
 
     h3.appendChild(h3Txt);
+    h3.addEventListener('click', doStuff(i));
     document.body.appendChild(h3);
- 
-    h3.addEventListener(`Accusation ${i}: I accuse ${FRIEND_NAME}, with the ${WEAPON_NAME} in the ${LOCATION_NAME!}`)
-}
+  }
 
-})
-
-      
-
-// h3.addEventListener('click', () => {
-
-//   alert()
-// }
+  function doStuff(i) {
+    return function () {
+      alert(`Accusation ${i}: I accuse ${friend[i % friend.length]}, with the ${weapon[i % weapon.length]} in the ${location[i % location.length]}`)
+    }
+  }
+})  
