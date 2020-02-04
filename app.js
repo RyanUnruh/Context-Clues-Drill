468988ce1afb9263660f9d80a1aa8fe9729e7c0b
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-02-04 10:52:25 -0600

    11AM

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+for (let index = 0; index < 100; index++) {
+  
+ 
+    var h3 = document.createElement('h3');
+    var h3Txt = document.createTextNode(`Accusation ${index}`);
+
+    h3.appendChild(h3Txt);
+    document.body.appendChild(h3);
+ 
+    h3.addEventListener(`Accusation ${i}: I accuse ${FRIEND_NAME}, with the ${WEAPON_NAME} in the ${LOCATION_NAME!}`)
+}
+
+})
+
+      
+
+// h3.addEventListener('click', () => {
+
+//   alert()
+// }
